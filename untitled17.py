523538fe09fee1bd84a6d8720785794b0bd3f4d7
Arthurs1216 <62162202+Arthurs1216@users.noreply.github.com>
2021-05-01 15:13:24 +0800

    Add files via upload

--- untitled17.py
+++ untitled17.py
@@ -1,23 +1,22 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Mar 27 14:31:09 2021
+Created on Sat May  1 14:56:49 2021
 
 @author: Arthu
 """
 
-import requests
 from bs4 import BeautifulSoup
-url = 'https://www.books.com.tw/web/sys_saletopb/books/'
-data = requests.get(url)
-soup = BeautifulSoup(data.text, 'html.parser')
-a = 1
-div = soup.find_all("img" , class_="cover")
-for each_divs in div[:20]:
-    img = each_divs['src']
-    bookname = each_divs["alt"]
-    
-    img = requests.get(img)
-    
-    with open(bookname + ".jpg" ,"wb") as f:
-        f.write(img.content + str(a))
-        a +=1
+from selenium import webdriver
+import time
+chrome = webdriver.Chrome()
+chrome.get('https://ecshweb.pchome.com.tw/search/v3.3/?q=iphone')
+time.sleep(3)
+for i in range(2):
+    chrome.execute_script('window.scrollTo(0,document.body.scrollHeight);')
+    time.sleep(1)
+time.sleep(5)
+pageSrc = chrome.page_source
+soup =  BeautifulSoup(pageSrc,'html.parser')
+chrome.close()
+for prods in soup.find_all('h5', class_='prod_name'):
+    print(prods.text)
